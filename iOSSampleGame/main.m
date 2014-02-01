23601071977cd6cb65b28deb23358f25dce62879
Zhaochun Lu <zhaochunlu@ZhaochundeMacBook-Pro.local>
2014-02-01 14:32:23 +0800

    Initial Commit

---
+++ iOSSampleGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSSampleGame
+//
+//  Created by Zhaochun Lu on 14-2-1.
+//  Copyright (c) 2014年 Zhaochun Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iOSSampleGameTests/iOSSampleGameTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSSampleGameTests.m
+//  iOSSampleGameTests
+//
+//  Created by Zhaochun Lu on 14-2-1.
+//  Copyright (c) 2014年 Zhaochun Lu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSSampleGameTests : XCTestCase
+
+@end
+
+@implementation iOSSampleGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
